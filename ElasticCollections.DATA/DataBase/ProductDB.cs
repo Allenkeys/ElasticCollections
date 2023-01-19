f1cea62f8929da3ce117798aa237a4e4424b4bdc
Joshua Eze <ezejoshuac@gmail.com>
2023-01-19 12:32:04 +0100

    Add project files.

---
+++ ElasticCollections.DATA/DataBase/ProductDB.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using ElasticCollections.DATA.Models;
+
+namespace ElasticCollections.DATA.DataBase
+{
+    internal class ProductDB
+    {
+        public IList<Product> Products { get; } = new List<Product>
+        {
+             new Product
+             {
+                 Id = 1,
+                 Name = "Dell XPS",
+                 Quantity = 20,
+                 Price = 1000.99,
+                 Category = "PCs",
+                 OrderCount = 5
+             },
+             new Product
+             {
+                 Id = 2,
+                 Name = "Hp ProBook",
+                 Quantity = 25,
+                 Price = 500.99,
+                 Category = "PCs",
+                 OrderCount = 6
+             },
+             new Product
+             {
+                 Id = 3,
+                 Name = "Ergonomic Chair",
+                 Quantity = 100,
+                 Price = 200.99,
+                 Category = "Chairs",
+                 OrderCount = 10
+             },
+             new Product
+             {
+                 Id = 4,
+                 Name = "Massager",
+                 Quantity = 20,
+                 Price = 2100.99,
+                 Category = "Chairs",
+                 OrderCount = 25
+             },
+             new Product
+             {
+                 Id = 5,
+                 Name = "Plywood Table",
+                 Quantity = 55,
+                 Price = 520.99,
+                 Category = "Tables",
+                 OrderCount = 8
+             },
+             new Product
+             {
+                 Id = 6 ,
+                 Name = "Adjustable Table",
+                 Quantity = 20,
+                 Price = 1200.99,
+                 Category = "Tables",
+                 OrderCount = 7
+             },
+
+        };
+
+    }
+}
---
+++ ElasticCollections.DATA/Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElasticCollections.DATA.Models
+{
+    internal class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+        public string Category { get; set; }
+        public int OrderCount { get; set; }
+
+        /*public Product(string name, int quantity, int orderCount)
+        {
+            Name = name;
+            Quantity = quantity;
+            OrderCount = orderCount;
+        }
+
+        public Product() { }*/
+
+    }
+}
