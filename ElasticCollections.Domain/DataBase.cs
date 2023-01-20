69f1eb915a10f22d292f7c338d20f582a9656f81
Joshua Eze <ezejoshuac@gmail.com>
2023-01-20 01:57:54 +0100

    Refactored Database implementation

--- ElasticCollections.Domain/DataBase.cs
+++ ElasticCollections.Domain/DataBase.cs
@@ -4,75 +4,36 @@ namespace ElasticCollections.Domain
 {
     public class DataBase
     {
-        public List<ExpandoObject> _products = new List<ExpandoObject>();
-        public void ProductGenerator()
+        public static dynamic _products = new List<dynamic>();
+        public static void ProductGenerator()
         {
-            dynamic product1 = new ExpandoObject();
-            product1.Id = 1;
-            product1.Name = "Dell XPS";
-            product1.Quantity = 20;
-            product1.Price = 1200.99;
-            product1.Category = "PCs";
-            product1.OrderCount = 35;
+            _products.Add(new ExpandoObject());
+            _products[0].Id = 1;
+            _products[0].Name = "Dell XPS";
+            _products[0].Quantity = 20;
+            _products[0].Price = 1200.99;
+            _products[0].Category = "PCs";
+            _products[0].OrderCount = 35;
 
-            dynamic product2 = new ExpandoObject();
-            product2.Id = 2;
-            product2.Name = "Ergonomic Chair";
-            product2.Quantity = 25;
-            product2.Price = 1100.99;
-            product2.Category = "PCs";
-            product2.OrderCount = 357;
+            _products.Add(new ExpandoObject());
+            _products[1].Id = 2;
+            _products[1].Name = "Ergonomic Chair";
+            _products[1].Quantity = 25;
+            _products[1].Price = 1100.99;
+            _products[1].Category = "PCs";
+            _products[1].OrderCount = 357;
 
-            dynamic product3 = new ExpandoObject();
-            product3.Id = 3;
-            product3.Name = "Oakwood Table";
-            product3.Quantity = 30;
-            product3.Price = 4000.99;
-            product3.Category = "PCs";
-            product3.OrderCount = 368;
+            _products.Add(new ExpandoObject());
+            _products[2].Id = 3;
+            _products[2].Name = "Oakwood Table";
+            _products[2].Quantity = 30;
+            _products[2].Price = 4000.99;
+            _products[2].Category = "PCs";
+            _products[2].OrderCount = 368;
 
-
-            _products.Add(product1);
-            _products.Add(product2);
-            _products.Add(product3);
-        }
-
-        public void Display()
-        {
-            _products.Clear();
-            ProductGenerator();
-            foreach (dynamic product in _products)
-            {
-                Console.WriteLine($"{product.Id}, {product.Name}, {product.Quantity}, ${product.Price}, {product.Category}, {product.OrderCount}");
-            }
-        }
-
-        public void Display(string arg1, string arg2, string arg3)
-        {
-            _products.Clear();
-            ProductGenerator();
-            foreach (dynamic product in _products)
-            {   
-                foreach(var property in(IDictionary<string, object>)product)
-                {
-                    if(property.Key == arg1)
-                    {
-                        Console.Write($"\n{property.Value}, ");
-                    }
-                    if (property.Key == arg2)
-                    {
-                        Console.Write($"{property.Value}, ");
-                    }
-                    if (property.Key == arg3)
-                    {
-                        Console.Write($"{property.Value}, ");
-                        Console.WriteLine();
-                    }
-                }
-            }
-            
         }
 
     }
 
 }
+
